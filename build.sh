1422b4c093d403c68d7bc4f2574ea28a8e01094a
Kien Truong <ktruong29@csu.fullerton.edu>
2020-08-14 14:27:53 -0700

    Second commit

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 #/*******************************************************************************
 # *Author: Kien Truong
-# *Assignment 3
-# *CPSC 223N
 #******************************************************************************/
 
 echo First remove old binary files
@@ -12,7 +10,7 @@ rm *.exe
 echo View the list of source files
 ls -l
 
-echo Compile RicochetBall.cs to create the file: TravelingBall.dll
+echo Compile RicochetBall.cs to create the file: Ricochet.dll
 mcs -target:library -r:System.Drawing.dll -r:System.Windows.Forms.dll -out:RicochetBall.dll RicochetBall.cs
 
 echo Link the previously created dll file to create an executable file.
@@ -21,7 +19,7 @@ mcs -r:System -r:System.Windows.Forms -r:RicochetBall.dll -out:Ricochet.exe main
 echo View the list of files in the current folder
 ls -l
 
-echo Run the Assignment 3 program.
+echo Run the Ricochet program.
 ./Ricochet.exe
 
 echo The script has terminated.
